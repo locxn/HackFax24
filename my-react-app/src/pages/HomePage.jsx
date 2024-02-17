b57e2a72b493673131ce8ad09e246601dd5edf77
Loc Nguyen <101442580+locxn@users.noreply.github.com>
2024-02-17 18:55:18 -0500

    update app

---
+++ my-react-app/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+
+
+function HomePage(){
+
+    return(
+        <>
+            <div>
+                Hello this is the home page
+            </div>
+        </>
+    )
+}
+
+export default HomePage
