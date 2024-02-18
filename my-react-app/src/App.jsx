d3fc25c48018b6e23374e9e690ffc41822903784
Loc Nguyen <101442580+locxn@users.noreply.github.com>
2024-02-17 20:06:56 -0500

    2

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,36 +1,21 @@
 import { useState } from 'react'
 // import './App.css'
+import Header from './Header.jsx'
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
 import {Link} from 'react-router-dom'
 
 
+
 function App() {
     // const [page, setPage] = useState(0)
     return (
         <>
-            
-            {/* <div>
-                <BrowserRouter>
-                    <Routes>
-                        <Route index element = {<HomePage/>}/>
-                        <Route path = '/home' element = {<HomePage/>} />
-                    </Routes>
-                </BrowserRouter>
-            </div> */}
-
-            {/* <nav>
-                <ul>
-                    <li>
-                        <Link to= "/home">Home</Link>
-                    </li>
-                    <li>
-                        <Link to= "/about">about</Link>
-
-                    </li>
-                </ul>
-            </nav> */}
+            <p>Hello</p>
+            {/* <Header/>s */}
+           
 
         </>
     )
--- my-react-app/src/pages/AboutPage.jsx
+++ my-react-app/src/pages/AboutPage.jsx
@@ -1,11 +1,10 @@
-import Header from '../components/Header.jsx'
+
 
 
 function AboutPage(){
 
     return(
         <>
-            <Header/>
             <h2>About page</h2>
         </>
     )
